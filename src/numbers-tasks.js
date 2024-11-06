c0510541acbf8b0837c94ffc31c87a7704ceacb4
YamatashiOne <boogieleet@gmail.com>
2024-11-06 09:41:32 +0100

    comment task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -249,6 +249,21 @@ function getCube(/* num */) {
  */
 function getFibonacciNumber(/* index */) {
   throw new Error('Not implemented');
+  // if (index === 0) {
+  //   return 0;
+  // }
+  // if (index === 1) {
+  //   return 1;
+  // }
+  // let a = 0;
+  // let b = 1;
+  // for (let i = 2; i <= index; i + 1) {
+  //   const next = a + b;
+  //   a = b;
+  //   b = next;
+  // }
+
+  // return b;
 }
 
 /**
